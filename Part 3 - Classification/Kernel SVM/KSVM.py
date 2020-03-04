4a6ce89abbc8e67974fec1b3236efa9948df0c22
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-03-05 00:11:11 +0530

    KSVM

--- Part 3 - Classification/Kernel SVM/KSVM.py
+++ Part 3 - Classification/Kernel SVM/KSVM.py
@@ -1,40 +1,41 @@
-# Kernel SVM
-
-# Importing the libraries
+#importing Libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+#importing datasets and making matrices
 dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
-y = dataset.iloc[:, 4].values
+x = dataset.iloc [ : , [2,3]].values
+y = dataset.iloc [ : ,4 ].values
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+# Splitting the datasets into training set and test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x , y, test_size= 0.25, random_state = 0 )
 
-# Fitting Kernel SVM to the Training set
+#feature scaling
+from  sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+
+#fitting Kernel Svm model to the traning test
+
 from sklearn.svm import SVC
-classifier = SVC(kernel = 'rbf', random_state = 0)
-classifier.fit(X_train, y_train)
+classifier = SVC(kernel = 'rbf',random_state = 0)
+classifier.fit(x_train, y_train)
 
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
+#predicting the test set result
 
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
+y_pred = classifier.predict(x_test)
+
+#making the confusion matrix
+from sklearn.metrics  import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
-# Visualising the Training set results
+#visualising results
 from matplotlib.colors import ListedColormap
-X_set, y_set = X_train, y_train
+X_set, y_set = x_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
@@ -44,15 +45,15 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Kernel SVM (Training set)')
+plt.title('Logistic Regression (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
 
-# Visualising the Test set results
+#test set result
 from matplotlib.colors import ListedColormap
-X_set, y_set = X_test, y_test
+X_set, y_set = x_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
@@ -62,7 +63,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Kernel SVM (Test set)')
+plt.title('Logistic Regression (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
